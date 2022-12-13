3d4f703c3f899dd606b9668a237bb4648874e7a9
NazarovSV <badger.lock@yandex.ru>
2022-12-13 11:39:20 +0300

    Add flash message/errors

--- spec/features/vacancy/vacancy_spec.rb
+++ spec/features/vacancy/vacancy_spec.rb
@@ -18,10 +18,64 @@ feature 'Any user can add new vacancy', '
 
       click_on 'Create'
 
+      expect(page).to have_content 'Your vacancy successfully created.'
+
       expect(page).to have_content vacancy.title
       expect(page).to have_content vacancy.description
       expect(page).to have_content vacancy.phone
       expect(page).to have_content vacancy.email
+    end
+
+    scenario 'can add new vacancy without phone' do
+      visit new_vacancy_path
+
+      fill_in 'Title', with: vacancy.title
+      fill_in 'Description', with: vacancy.description
+      fill_in 'Email', with: vacancy.email
+
+      click_on 'Create'
+
+
+      expect(page).to have_content 'Your vacancy successfully created.'
+
+      expect(page).to have_content vacancy.title
+      expect(page).to have_content vacancy.description
+      expect(page).to have_content vacancy.email
+    end
+
+
+
+    scenario 'can`t add new vacancy without title' do
+      visit new_vacancy_path
+
+      fill_in 'Description', with: vacancy.description
+      fill_in 'Email', with: vacancy.email
+
+      click_on 'Create'
+
+      expect(page).to have_content "Title can't be blank"
+    end
+
+    scenario 'can`t add new vacancy without description' do
+      visit new_vacancy_path
+
+      fill_in 'Title', with: vacancy.title
+      fill_in 'Email', with: vacancy.email
+
+      click_on 'Create'
+
+      expect(page).to have_content "Description can't be blank"
+    end
+
+    scenario 'can`t add new vacancy without email' do
+      visit new_vacancy_path
+
+      fill_in 'Title', with: vacancy.title
+      fill_in 'Description', with: vacancy.description
+
+      click_on 'Create'
+
+      expect(page).to have_content "Email can't be blank"
     end
   end
 end
