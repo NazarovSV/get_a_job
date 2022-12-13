ddf7b0d41b4aecf79498e44566f0f476db5184c4
NazarovSV <badger.lock@yandex.ru>
2022-12-13 14:28:55 +0300

    add email validation and some refactoring

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,10 +1,11 @@
 # frozen_string_literal: true
 
 module ApplicationHelper
+
   def flash_message(type)
     return unless flash[type]
 
-    content_tag :div, class: "alert #{message_type(type)} alert-dismissible fade show", role: 'alert' do
+    content_tag :div, class: "alert #{message_type(type.to_sym)} alert-dismissible fade show", role: 'alert' do
       content_tag(:span, flash[type]) +
         button_tag('', type: 'button', class: 'btn-close', data: { bs_dismiss: 'alert' }, aria: { label: 'Close' })
     end
@@ -12,12 +13,12 @@ module ApplicationHelper
 
   private
 
+  MESSAGE_TYPE = {
+    notice: 'alert-success',
+    alert: 'alert-danger'
+  }.freeze
+
   def message_type(type)
-    case type
-    when 'notice' then 'alert-success'
-    when 'alert' then 'alert-danger'
-    else
-      'alert-info'
-    end
+    MESSAGE_TYPE.has_key?(type) ? MESSAGE_TYPE[type] : 'alert-info'
   end
 end
