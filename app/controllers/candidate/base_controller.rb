9d0de619d1a54b206ad05742e6def8e1915280e7
NazarovSV <73112716+NazarovSV@users.noreply.github.com>
2023-01-23 12:21:10 +0300

    Revert "wip"
    This reverts commit 947669a26430cabaacd533d48fd223883bf07195.

--- app/controllers/candidate/base_controller.rb
+++
@@ -1,9 +0,0 @@
-# frozen_string_literal: true
-
-module Candidate
-  class BaseController < ApplicationController
-    before_action :authenticate_employee!
-
-    alias current_user current_employee
-  end
-end
